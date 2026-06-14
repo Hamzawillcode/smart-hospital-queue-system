29d33b183d36ee17d1dce2de985c224b1566c028
Hamza Rouf <hamzarouf@Hamzas-MacBook-Air.local>
2026-06-15 02:56:58 +0530

    patient-service project setup with hello endpoint

---
+++ patient-service/src/main/java/com/hospital/patient/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.hospital.patient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Patient service is running!";
+    }
+}
